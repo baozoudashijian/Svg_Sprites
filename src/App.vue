c056fdbcc89cf4e52c56cfb556ef1a5b1f51410f
ronger Zhang <1787009521@qq.com>
2023-01-12 11:09:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const x = ref(0)
+</script>
+
+<template>
+  <main class="main">
+    <svg>
+      <use xlink:href="#logo"></use>
+    </svg>
+  </main>
+</template>
+
+<style>
+  main > svg {
+      width: 64px;
+      height: 69px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@svgstore'
+
+createApp(App).mount('#app')
